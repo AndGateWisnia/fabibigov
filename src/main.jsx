b2b21161c1a0caf715aceaf14c2a7f940c6f51b4
2Fabi1 <158741663+2Fabi1@users.noreply.github.com>
2026-04-28 12:16:04 +0200

    Replace FabibiGov import with App import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import FabibiGov from "./FabibiGov.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

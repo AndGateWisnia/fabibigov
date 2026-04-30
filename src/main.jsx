0bf1bd27bb984e8b2b65892be7932d91604a75ab
2Fabi1 <158741663+2Fabi1@users.noreply.github.com>
2026-04-30 09:20:37 +0200

    Add 'type' prop to App component in routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,12 @@ ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/damskie">
-          <Route path="imiona" element={<App ids={[1159538, 63899, 54100, 44824, 36394, 28021, 21455]} sexLetter="K"/>}/>
-          <Route path="nazwiska" element={<App ids={[1148811]} sexLetter="K"/>}/>
+          <Route path="imiona" element={<App ids={[1159538, 63899, 54100, 44824, 36394, 28021, 21455]} sexLetter="K" type="imie"/>}/>
+          <Route path="nazwiska" element={<App ids={[1148811]} sexLetter="K" type="nazwisko"/>}/>
       </Route> 
       <Route path="/meskie">
-          <Route path="imiona" element={<App ids={[1159536, 63900, 54099, 44825, 36398, 28020, 21454]} sexLetter="M"/>}/>
-          <Route path="nazwiska" element={<App ids={[1148808]} sexLetter="M"/>}/>
+          <Route path="imiona" element={<App ids={[1159536, 63900, 54099, 44825, 36398, 28020, 21454]} sexLetter="M" type="imie"/>}/>
+          <Route path="nazwiska" element={<App ids={[1148808]} sexLetter="M" type="nazwisko"/>}/>
       </Route> 
     </Routes>
   </BrowserRouter>,

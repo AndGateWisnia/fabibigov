6f0e61fa697efddd81d806da941832a81eb20426
AndGateWisnia <andrzej.krakowski@tm1.edu.pl>
2026-05-12 10:58:33 +0200

    gh-pages compatible

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
 import App from "./App.jsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/damskie">
           <Route path="imiona" element={<App ids={[1159538, 63899, 54100, 44824, 36394, 28021, 21455]} sexLetter="K" type="imie"/>}/>
@@ -17,5 +17,5 @@ ReactDOM.createRoot(root).render(
           <Route path="nazwiska" element={<App ids={[1148808]} sexLetter="M" type="nazwisko"/>}/>
       </Route> 
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
